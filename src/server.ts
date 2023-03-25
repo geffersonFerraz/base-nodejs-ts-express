62ee30b4e1d1154005497b5a1df55c73590a704f
Gefferson Ferraz <me@gefferson.com.br>
2023-03-24 21:00:57 -0300

    fix: open pdf

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,25 @@ dotenv.config({path: process.env.NODE_ENV === "test" ? "./test/.env-test" : ".en
 
 const server: Express = express();
 const port: number = _.toNumber(process.env.PORT);
-
+server.use(express.json()) 
 server.get('/', (req: Request, res: Response) => {
     res.send({ 'message': 'Express + TypeScript Server' });
+});
+
+
+server.get('/endpointOne', (req: Request, res: Response) => {
+    const param = req.headers['parametro-header'];
+    res.send({ 'message': param });
+});
+
+server.get('/endpointTwo', (req: Request, res: Response) => {
+    const param = req.query['parametro-query'];
+    res.send({ 'message': param });
+});
+
+server.get('/endpointThree', (req: Request, res: Response) => {
+    const param = req.body
+    res.send({ 'message': param });
 });
 
 export default server.listen(port, () => {
